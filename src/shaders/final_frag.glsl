3ed0c221d859519dcdfbf4831f8c54580a06dd2e
Eero Lehtinen <eero@eerolehtinen.fi>
2024-10-16 21:51:34 +0300

    Always show final color without alpha

--- src/shaders/final_frag.glsl
+++ src/shaders/final_frag.glsl
@@ -4,11 +4,21 @@ uniform vec4 color;
 void main() {
 	vec4 cb = checkerboard(0.1, 0.1);
 
-	vec4 color = uv.x < 0.5 ? prev_color : color;
+	vec4 c = vec4(0);
 
-	color = blend_premultiplied(
+	if (uv.x < 0.25) {
+		c = prev_color;
+	} else if (uv.x < 0.5) {
+		c = vec4(prev_color.rgb, 1);
+	} else if (uv.x < 0.75) {
+		c = vec4(color.rgb, 1);
+	} else {
+		c = color;
+	}
+
+	vec4 color = blend_premultiplied(
 		premultiply(cb), 
-		premultiply(color)
+		premultiply(c)
 	);
 
     FragColor = color;
